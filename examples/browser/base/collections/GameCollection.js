31249ad5378d5d29b526eeab843b0a5e2f08c01b
BambiHaber <haber.ofer@gmail.com>
2018-05-18 22:01:51 +0200

    BaseMVC first commit - Post node publish commit

--- examples/browser/base/collections/GameCollection.js
+++ examples/browser/base/collections/GameCollection.js
@@ -7,5 +7,4 @@ export class GameCollection extends BaseCollection {
 			model: GameModel
 		}))
 	}
-
 }
--- examples/browser/base/templates/GamesTemplate.js
+++ examples/browser/base/templates/GamesTemplate.js
@@ -1,24 +1,24 @@
 export default `
 		<thead class="tbl-header">
 		<th>Game</th><th>Released</th><th>By</th><th>Regions</th>
-</thead>
-<tbody class="tbl-content">
+		</thead>
+		<tbody class="tbl-content">
 		<% for (var i = 0; i < this.length; i++) { %>
 		<tr>
-		<td>
-	 <%= this[i].title %>
-		</td>
-		<td>
-		<%= this[i].year %>
-		</td>
-		<td>
-		<%=this[i].publisher%>
-		</td>
-		<td>
-		<% this[i].regions.forEach((region)=>{ %>
-		<%=region%>
-		<% }) %>
-		</td>
+			<td>
+				<%= this[i].title %>
+			</td>
+			<td>
+				<%= this[i].year %>
+			</td>
+			<td>
+				<%=this[i].publisher%>
+			</td>
+			<td>
+				<% this[i].regions.forEach((region)=>{ %>
+					<%=region%>
+				<% }) %>
+			</td>
 		</tr>
 		<% } %>
 		</tbody>
